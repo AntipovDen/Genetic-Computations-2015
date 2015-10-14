72d7bd892eeb8fb4e6c9b2eba4bf8aeda1d4c79c
AntipovDen <antipovden@yandex.ru>
2015-10-14 11:58:53 +0300

    lol python works

--- lab1/B.py
+++ lab1/B.py
@@ -3,7 +3,6 @@ from time import time
 __author__ = 'Den'
  
 from random import random, randint
-from collections import deque
 
 
 this_is_the_end = False
@@ -66,44 +65,47 @@ def run(f):
     new_pop = [[0] * n for i in range(pop_size)]
     values = [f(x) for x in pop]
     new_values = [0] * pop_size
-    best = deque([9000.] * (5 * n - 1) + [min(values)])
-    new_best = best[0]
+    # best = deque([9000.] * (5 * n - 1) + [min(values)])
+    # new_best = best[0]
+    best = 9000.
     while(True):
         for i in range(pop_size):
-            a, b, c = randint(0, pop_size - 1), randint(0, pop_size - 2), randint(0, pop_size - 3)
-            if b >= a: b += 1
-            if c >= min(a, b): c += 1
-            if c >= max(a, b): c += 1
+            b, c = randint(0, pop_size - 2), randint(0, pop_size - 3)
+            if b >= i: b += 1
+            if c >= min(i, b): c += 1
+            if c >= max(i, b): c += 1
             # gen new candidate
             for j in range(n):
-                if random() < mutation:
-                    new_pop[i][j] = random() * 20. - 10.
-                elif random() < crossover:
-                    new_pop[i][j] = pop[a][j] + alpha * (pop[b][j] - pop[c][j])
+                if random() < crossover:
+                    new_pop[i][j] = pop[i][j] + alpha * (pop[b][j] - pop[c][j])
                     if new_pop[i][j] > 10.: new_pop[i][j] = 10.
                     if new_pop[i][j] < -10.: new_pop[i][j] = -10.
                 else:
                     new_pop[i][j] = pop[i][j]
             # we've got new candidate
             new_value = f(new_pop[i])
-            if this_is_the_end:
-                print(best.pop())
-                exit(0)
+            # if this_is_the_end:
+            #     print(best)
+            #     exit(0)
             if new_value < values[i]:
                 new_values[i] = new_value
-                if new_best > new_value: new_best = new_value
+                if best > new_value:
+                    best = new_value
             else:
                 new_values[i] = values[i]
                 for j in range(n):
                     new_pop[i][j] = pop[i][j]
         # we dont want to stay at local minimum
-        if best.popleft() - new_best < 0.1:
-            return new_best # , new_pop[argmin(new_values)]
-        best.append(new_best)
-        values, pop, new_values, new_pop = new_values, new_pop, values, pop
+        # if best.popleft() - new_best < 0.1:
+        #     return new_best # , new_pop[argmin(new_values)]
+        # best.append(new_best)
+        values, new_values = new_values, values
+        pop, new_pop = new_pop, pop
+#
+# while(True):
+#     t = time()
+#     res = run(dim10_test_f)
+#     alpha += 0.1
+#     print("run result:", res, "time:", time() - t, sep='\t')
 
-while(True):
-    t = time()
-    res = run(dim10_test_f)
-    alpha += 0.1
-    print("run result:", res, "time:", time() - t, sep='\t')
+run(real_f)
